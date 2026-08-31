99748bc8fedbd98594298dc2a2f7f2fe8aad154d
Raphael Vinz Tina <vinztina514@gmail.com>
2026-08-31 21:42:52 +0800

    feat: finish part03exercise26

--- tmcdata/mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
+++ tmcdata/mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class Login {
@@ -6,5 +5,17 @@ public class Login {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter username: ");
+        String user = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String pass = scanner.nextLine();
+        
+        if ((user.equals("alex") && pass.equals("sunshine")) || (user.equals("emma") && pass.equals("haskell"))) {
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
+
+        scanner.close();
     }
 }
--- tmcdata/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ tmcdata/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -6,6 +6,37 @@ public class LineByLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String phrase = scanner.nextLine();
 
+            if (phrase.equals("")) {
+                break;
+            }
+            String[] pieces = phrase.split(" ");
+            for (String piece : pieces) {
+                System.out.println(piece);
+            }
+        }
     }
 }
+
+// The code I provided before I got it correct
+/*
+ * (import java.util.Scanner;
+ * 
+ * public class LineByLine {
+ * 
+ * public static void main(String[] args) {
+ * Scanner scanner = new Scanner(System.in);
+ * 
+ * System.out.print("Enter phrase: ");
+ * String phrase = scanner.nextLine();
+ * 
+ * String[] pieces = phrase.split(" ");
+ * for(int i=0; i<pieces.length; i++){
+ * System.out.println(pieces[i]);
+ * }
+ * 
+ * }
+ * }
+ */
