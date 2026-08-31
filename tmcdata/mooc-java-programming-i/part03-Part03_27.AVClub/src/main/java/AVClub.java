fa1d715982b69799016c34b11d692f37385ae572
Raphael Vinz Tina <vinztina514@gmail.com>
2026-08-31 22:01:28 +0800

    feat: finish part03exercise28

--- tmcdata/mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ tmcdata/mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,7 +5,19 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String phrase = scanner.nextLine();
 
-
+            if (phrase.equals("")) {
+                break;
+            }
+            String[] pieces = phrase.split(" ");
+            for (String piece : pieces) {
+                if(piece.contains("av")){
+                    System.out.println(piece);
+                }
+            }
+        }
+        scanner.close();
     }
 }
