aa75a0ea48393a6abfd644b200f09d999cd275b8
Raphael Vinz Tina <vinztina514@gmail.com>
2026-08-31 20:26:14 +0800

    feat: finish part03exercise22

--- tmcdata/mooc-java-programming-i/part03-Part03_20.SumOfArray/src/main/java/SumOfArray.java
+++ tmcdata/mooc-java-programming-i/part03-Part03_20.SumOfArray/src/main/java/SumOfArray.java
@@ -4,7 +4,6 @@ public class SumOfArray {
     public static void main(String[] args) {
         int sum = 0;
         int[] array = {5, 1, 3, 4, 2};
-        System.out.println(sumOfNumbersInArray(array));
         sumOfNumbersInArray(array);
         System.out.println(sumOfNumbersInArray(array));
     }
--- tmcdata/mooc-java-programming-i/part03-Part03_22.PrintInStars/src/main/java/Printer.java
+++ tmcdata/mooc-java-programming-i/part03-Part03_22.PrintInStars/src/main/java/Printer.java
@@ -2,13 +2,23 @@
 public class Printer {
 
     public static void main(String[] args) {
-        // You can test the method here
         int[] array = {5, 1, 3, 4, 2};
         printArrayInStars(array);
     }
 
+    public static void printStars(int number) {
+        int i = 0;
+        while (i < number) {
+            System.out.print("*");
+            i++;
+        }
+        System.out.println("");
+    }
+
     public static void printArrayInStars(int[] array) {
-        // Write some code in here
+        for (int stars : array) {
+            printStars(stars); 
+        }
     }
 
 }
