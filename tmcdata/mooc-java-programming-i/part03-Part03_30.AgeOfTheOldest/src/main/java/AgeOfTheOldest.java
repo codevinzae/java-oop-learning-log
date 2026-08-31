65f93ffbf9c742c89729e29b54826223fff8c579
Raphael Vinz Tina <vinztina514@gmail.com>
2026-08-31 23:22:06 +0800

    feat: finish part03 overall

--- tmcdata/mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ tmcdata/mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class AgeOfTheOldest {
@@ -6,6 +5,26 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int oldest = -1; 
 
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] parts = input.split(",");
+
+            int age = Integer.valueOf(parts[1]);
+
+            if (age > oldest) {
+                oldest = age;
+            }
+        }
+
+        System.out.println("Age of the oldest: " + oldest);
+
+        scanner.close();
     }
 }
--- tmcdata/mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ tmcdata/mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -1,5 +1,3 @@
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PersonalDetails {
@@ -7,6 +5,34 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String longestName = "";
+        int sumOfBirthYears = 0;
+        int count = 0;
 
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] parts = input.split(",");
+            String name = parts[0];
+            int birthYear = Integer.valueOf(parts[1]);
+
+            if (name.length() > longestName.length()) {
+                longestName = name;
+            }
+
+            sumOfBirthYears += birthYear;
+            count++;
+        }
+
+        double average = (double) sumOfBirthYears / count;
+
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + average);
+
+        scanner.close();
     }
 }
