46f3910986833cb1a3d05a4b93769022ccae2765
Raphael Vinz Tina <vinztina514@gmail.com>
2026-09-02 22:04:50 +0800

    feat: finish part04exercise15

--- tmcdata/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ tmcdata/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -5,12 +5,33 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // you can write test code here
-        // however, remove all unnecessary code when doing the final parts of the exercise
+        Statistics totalSum = new Statistics();
+        Statistics evenSum = new Statistics();
+        Statistics oddSum = new Statistics();
 
-        // In order for the tests to work, the objects must be created in the
-        // correct order in the main program. First the object that tracks the total
-        // sum, secondly the object that tracks the sum of even numbers, 
-        // and lastly the one that tracks the sum of odd numbers!
+        System.out.println("Enter numbers: ");
+        
+        while (true){
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1){
+                break;
+            }
+
+        totalSum.addNumber(input);
+
+        if(input % 2 == 0){
+            evenSum.addNumber(input);
+        }else{
+            oddSum.addNumber(input);
+        }
+
+        }
+
+        //given block of codes from MOOC to print
+        System.out.println("Count: " + totalSum.getCount());
+        System.out.println("Sum: " + totalSum.sum());
+        System.out.println("Average: " + totalSum.average());
+        System.out.println("Sum of even numbers: " + evenSum.sum());
+        System.out.println("Sum of odd numbers: " + oddSum.sum());
     }
 }
