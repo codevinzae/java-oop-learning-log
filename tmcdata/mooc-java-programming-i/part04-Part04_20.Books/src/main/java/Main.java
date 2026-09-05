f6e9dc2447817f90a5e803f6aa45820ed9fb58d7
Raphael Vinz Tina <vinztina514@gmail.com>
2026-09-05 19:46:24 +0800

    feat: finish part04exercise19

--- tmcdata/mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
+++ tmcdata/mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
@@ -5,8 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         
-        // implement here the program that allows the user to enter 
-        // book information and to examine them
+       
 
     }
 }
