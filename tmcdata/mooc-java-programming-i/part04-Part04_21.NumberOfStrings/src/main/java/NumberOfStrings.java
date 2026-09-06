2f3ce168773f331afc9ff792234ff6c4fb9a3d19
Raphael Vinz Tina <vinztina514@gmail.com>
2026-09-06 15:29:15 +0800

    feat: finish part04exercise22

--- tmcdata/mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ tmcdata/mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -6,5 +6,15 @@ public class NumberOfStrings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int inputCount = 0;
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("end")){
+                break;
+            }
+            inputCount++; 
+        }
+        scanner.close();
+        System.out.println(inputCount);
     }
 }
--- tmcdata/mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ tmcdata/mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,15 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while(true){
+            String input = scanner.nextLine();
+                if (input.equals("end")){
+                    break;
+                }
+            int number = Integer.valueOf(input);
+            int cube = (int) Math.pow(number, 3);
+            System.out.println(cube);
+        }
+        scanner.close();
     }
 }
