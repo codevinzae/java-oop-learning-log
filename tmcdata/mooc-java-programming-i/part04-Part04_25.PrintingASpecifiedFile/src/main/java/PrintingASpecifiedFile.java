1e61625f821ef5d8611ae9f79b4ffa73daf6015d
Raphael Vinz Tina <vinztina514@gmail.com>
2026-09-06 15:51:44 +0800

    feat: finish part04exercise25

--- tmcdata/mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ tmcdata/mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -1,11 +1,21 @@
-
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
 
+        System.out.println("Which file should be printed?");
+        String fileName = userInput.nextLine();
+
+        try (Scanner fileScanner = new Scanner(Paths.get(fileName))) {
+            while (fileScanner.hasNextLine()) {
+                String row = fileScanner.nextLine();
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
