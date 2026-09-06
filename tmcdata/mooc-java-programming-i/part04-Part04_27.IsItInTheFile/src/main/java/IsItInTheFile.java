e6919f48946600426d6ec69a963ee2df4d69b6b9
Raphael Vinz Tina <vinztina514@gmail.com>
2026-09-06 20:03:25 +0800

    feat: finish part04exercise26

--- tmcdata/mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ tmcdata/mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -1,17 +1,37 @@
 
-import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class IsItInTheFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         System.out.println("Name of the file:");
         String file = scanner.nextLine();
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
+
+        boolean found = false;
+        try (Scanner fileScanner = new scanner(Paths.get(file))){
+            while(fileScanner.hasNextLine()){
+            String line = scanner.nextLine();
+            
+            if(line.equals(searchedFor)){
+                found = true;
+                break;
+            }
+
+            if(found){
+                System.out.println("Found!")
+            }else{
+                System.out.println
+            }
+
+            }
+        }catch (Exceptions e){
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 }
