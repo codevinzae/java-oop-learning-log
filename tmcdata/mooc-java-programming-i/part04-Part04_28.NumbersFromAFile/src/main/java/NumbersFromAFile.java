cc8bb80cd03589599a54d9c39a1a2262dd1fb1af
Raphael Vinz Tina <vinztina514@gmail.com>
2026-09-07 20:31:34 +0800

    feat: finish part04exercise28

--- tmcdata/mooc-java-programming-i/part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ tmcdata/mooc-java-programming-i/part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -7,26 +7,25 @@ public class NumbersFromAFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int count = 0;
         System.out.print("File? ");
         String file = scanner.nextLine();
         System.out.print("Lower bound? ");
         int lowerBound = Integer.valueOf(scanner.nextLine());
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
-        
-        try(Scanner fileScanner = new Scanner(Paths.get(file))){
-            while(fileScanner.hasNextLine()){
-                int number = Integer.valueOf(fileScanner.nextLine());
-                if(number <= upperBound && number>=lowerBound){
-                    count++;
+
+        int counter = 0;
+        try (Scanner fileReader = new Scanner(Paths.get(file))) {
+            while (fileReader.hasNextLine()) {
+                int num = Integer.valueOf(fileReader.nextLine());
+                if (num >= lowerBound && num <= upperBound) {
+                    counter++;
                 }
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Reading the file " + file + " failed.");
         }
-        scanner.close();
-        System.out.println("Numbers: " + count);
+        System.out.println("Numbers: " + counter);
     }
 
 }
