c619bd34b294810726ea23af62f0bbf273e1cd54
Raphael Vinz Tina <vinztina514@gmail.com>
2026-09-10 19:30:45 +0800

    feat: finish part04

--- tmcdata/mooc-java-programming-i/part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ tmcdata/mooc-java-programming-i/part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -1,4 +1,5 @@
 
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -26,11 +27,19 @@ public class StoringRecords {
             while(fileScanner.hasNextLine()){
                 String line = fileScanner.nextLine();
 
-                String[] parts = line.split(",");
-                String name = 
+                String[] detail = line.split(",");
+                String name = detail[0];
+                int age = Integer.valueOf(detail[1]);
+
+                for(Person person : persons){
+                    System.out.println(person);
+                }
+
+                persons.add(new Person(name, age));
             }
+        }catch(Exception e){
+            System.out.println("Reading the file " + file + " failed.");
         }
         return persons;
-
     }
 }
--- tmcdata/mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ tmcdata/mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -1,6 +1,4 @@
-
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SportStatistics {
@@ -8,6 +6,54 @@ public class SportStatistics {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-    }
+        System.out.println("File:");
+        String file = scan.nextLine();
 
+        System.out.println("Team:");
+        String team = scan.nextLine();
+
+        int gamesPlayed = 0;
+        int wins = 0;
+        int losses = 0;
+
+        try (Scanner fileScanner = new Scanner(Paths.get(file))) {
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                //[0] Home, [1] Visitor, [2] Home Pts, [3] Visitor Pts
+                String[] parts = line.split(",");
+                String homeTeam = parts[0];
+                String visitingTeam = parts[1];
+                int homePoints = Integer.valueOf(parts[2]);
+                int visitingPoints = Integer.valueOf(parts[3]);
+
+                
+                if (homeTeam.equals(team)) {
+                    gamesPlayed++;
+                    if (homePoints > visitingPoints) {
+                        wins++;
+                    } else {
+                        losses++;
+                    }
+                } else if (visitingTeam.equals(team)) {
+                    gamesPlayed++;
+                    if (visitingPoints > homePoints) {
+                        wins++;
+                    } else {
+                        losses++;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        System.out.println("Games: " + gamesPlayed);
+        System.out.println("Wins: " + wins);
+        System.out.println("Losses: " + losses);
+    }
 }
